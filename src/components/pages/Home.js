1b88f9eb102ac8131991d522200eb29651c15127
rjwalden <ryan@rosenblatt.ai>
2022-02-16 02:07:42 -0500

    bigger image

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,11 +1,13 @@
 import React from 'react'
 
 import Header from '../misc/Header'
+import Clients from '../misc/Clients'
 
 const Home = () => {
     return (
         <React.Fragment>
             <Header />
+            <Clients />
         </React.Fragment>
     )
 }
